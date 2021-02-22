f15de96f3c5e5b1a645027104447f1077af422e3
MSHE97 <sherzod.mustafokulov@mail.ru>
2021-02-22 17:34:59 +0500

    Добавлены типы

---
+++ pkg/types/types.go
@@ -0,0 +1,40 @@
+package types
+
+// Money - денежная суммы в минимальных еденицах (дирамы, копейки, центы и т.д.)
+type Money int64
+
+// Currency - код валюты
+type Currency string
+
+// Коды валют
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+	EUR Currency = "EUR"
+)
+
+//PAN номер карты
+type PAN string
+
+// Card - содержит информацию о платёжной карте
+type Card struct {
+	ID 			int
+	PAN
+	Balance 	Money
+	Currency
+	Color 		string
+	Name 		string
+	Active 		bool
+}
+
+type Payment struct {
+	ID int
+	Amount Money
+}
+
+type PaymentSource struct {
+	Type string // 'card'
+    Number string // номер вида '5058 xxxx xxxx 8888' 
+	Balance Money // баланс в дирамах
+}
